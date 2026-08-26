ea8ac1b6b0dc449b848804df00f1076a1f93e225
latea12011-arch <latea12011@gmail.com>
2026-08-26 22:16:58 +0800

    use applicant header space for expected salary

--- erp-v2/assets/app.js
+++ erp-v2/assets/app.js
@@ -381,7 +381,7 @@
       <h2>一、應徵與基本資料</h2>
       <table class="basic-info">
         <colgroup><col style="width:11%"><col style="width:18%"><col style="width:11%"><col style="width:18%"><col style="width:11%"><col style="width:13%"><col style="width:18%"></colgroup>
-        <tr><th>應徵日期</th><td colspan="5" class="birth-date">民國 ______ 年 ______ 月 ______ 日</td><td class="photo-box" rowspan="5">二吋照片<br>黏貼處</td></tr>
+        <tr><th>應徵日期</th><td colspan="2" class="birth-date">民國 ____ 年 ____ 月 ____ 日</td><th>期望薪資</th><td colspan="2">NT$ ______________／月　□ 可面議</td><td class="photo-box" rowspan="5">二吋照片<br>黏貼處</td></tr>
         <tr><th>應徵職位</th><td colspan="5" class="check-line">□ 保全　□ 機動保全　□ 行政保全　□ 總幹事　□ 秘書　□ 督導　□ 業務</td></tr>
         <tr><th>姓名</th><td></td><th>性別</th><td>□ 男　□ 女　□ 其他</td><th>出生日期</th><td class="birth-date">民國　　年　　月　　日</td></tr>
         <tr><th>身分證字號</th><td></td><th>聯絡電話</th><td></td><th>Email</th><td></td></tr>
@@ -393,7 +393,6 @@
       <table>
         <tr><th>可配合班別</th><td colspan="5" class="check-line">□ 日班　□ 夜班　□ 輪班　□ 假日　□ 現金班　□ 臨時支援　□ 其他：________________</td></tr>
         <tr><th>可到職日期</th><td></td><th>希望服務區域／案場</th><td colspan="3"></td></tr>
-        <tr><th>期望薪資</th><td colspan="5">新臺幣　　　　　　　　　元／月　　□ 可面議</td></tr>
         <tr><th>交通方式</th><td colspan="2">□ 步行　□ 自行車　□ 機車　□ 汽車　□ 大眾運輸　□ 其他</td><th>駕照類別</th><td colspan="2">________________________</td></tr>
         <tr><th>總幹事證號</th><td colspan="2"></td><th>其他專業證照</th><td colspan="2"></td></tr>
         <tr><th>良民證繳交</th><td colspan="5" class="check-line">□ 已繳交　□ 尚未繳交　□ 可於錄取後補交</td></tr>
